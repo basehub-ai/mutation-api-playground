aeda7e9027b1254330c6ad3545f994a41edb8661
Joaquin Montes <joaco.rm512@gmail.com>
2024-04-11 14:18:21 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { basehub } from "basehub";
-import type { Transaction } from "basehub/api-transaction";
 import { FeatureForm } from "@/components/feature-form";
+import { FormWrapper } from "@/components/form-wrapper";
 
 export default async function Home() {
   const query = await basehub({ cache: "no-store" }).query({
@@ -13,56 +13,9 @@ export default async function Home() {
 
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <form
-        action={async (data) => {
-          "use server";
-
-          const name = data.get("name")?.toString();
-          const content = data.get("content")?.toString();
-          const isHighlighted = data.get("highlighted") === "on";
-
-          const collectionId = query.homepage.heroFeatures._id;
-
-          console.log({ name, content, isHighlighted, collectionId });
-
-          const { transaction } = await basehub().mutation({
-            transaction: {
-              __args: {
-                data: JSON.stringify({
-                  type: "create",
-                  parentId: collectionId,
-                  data: {
-                    type: "instance",
-                    title: "",
-                    value: [
-                      {
-                        type: "text",
-                        value: name ?? null,
-                      },
-                      {
-                        type: "rich-text",
-                        value: {
-                          format: "markdown",
-                          value: content ?? "",
-                        },
-                      },
-                      {
-                        type: "boolean",
-                        value: isHighlighted ?? null,
-                      },
-                    ],
-                  },
-                } satisfies Transaction),
-                // autoCommit: "This was committed via API.",
-              },
-            },
-          });
-
-          return transaction;
-        }}
-      >
+      <FormWrapper collectionId={query.homepage.heroFeatures._id}>
         <FeatureForm />
-      </form>
+      </FormWrapper>
     </main>
   );
 }
